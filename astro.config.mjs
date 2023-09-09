3fde2b8ebfbee0d9f474003829e1bda41385ce9e
Oyster Lee <oysterd3@gmail.com>
2023-09-10 07:39:27 +0800

    chore: add site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://oysterlee.dev',
   integrations: [vue(), sitemap(), mdx(), UnoCSS({
     injectReset: '@unocss/reset/tailwind.css',
   })]
