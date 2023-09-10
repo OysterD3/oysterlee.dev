1edc5741a3de4f4fdb24d225d1b9d025cce438e6
Oyster Lee <oysterd3@gmail.com>
2023-09-10 15:51:47 +0800

    refactor: reusable blog header

---
+++ src/utils/calculate-reading-time.ts
@@ -0,0 +1,5 @@
+export const calculateReadingTime = (text: string) => {
+  const wordsPerMinute = 200;
+  const numberOfWords = text.split(/\s/g).length;
+  return Math.ceil(numberOfWords / wordsPerMinute);
+};
